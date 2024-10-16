74c507f901a8b443e37c2bf2c235331eb4869c38
artem <art3mk4@gmail.com>
2024-10-16 13:01:56 +0600

    added authorization

--- start/routes.ts
+++ start/routes.ts
@@ -10,6 +10,13 @@
 import router from '@adonisjs/core/services/router'
 import {middleware} from "#start/kernel";
 
+const AuthController = () => import('#controllers/auth_controller')
+
+router.group(() => {
+  router.post('register', [AuthController, 'register'])
+  router.post('login', [AuthController, 'login'])
+}).prefix('user')
+
 router.get('/', async () => {
   return {
     hello: 'world',
