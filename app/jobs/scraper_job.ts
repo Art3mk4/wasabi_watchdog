5f88010b36bfb201ee67d4948958c005f022c582
artem <art3mk4@gmail.com>
2024-10-16 17:42:39 +0600

    added scheduler, scraper job and review model, controller, migration

---
+++ app/jobs/scraper_job.ts
@@ -0,0 +1,11 @@
+
+import { BaseJob } from 'adonis-resque'
+import logger from '@adonisjs/core/services/logger'
+export default class ScraperJob extends BaseJob {
+  cron = '*/1 * * * * *'
+  // enqueue every five minutes
+  interval = '5m'
+    async perform() {
+      logger.info(`Repeater every 5 minutes / every seconds`)
+    }
+}
---
+++ app/models/review.ts
@@ -0,0 +1,34 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export enum ReviewSource {
+  DoubleGIS = '2gis',
+  YandexMaps = 'yandex_map',
+  DeliveryClub = 'delivery_club'
+}
+
+export default class Review extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare name: string | null
+
+  @column()
+  declare rating: number
+
+  @column()
+  declare text: string | null
+
+  @column()
+  declare date: DateTime
+
+  @column()
+  declare source: ReviewSource
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
