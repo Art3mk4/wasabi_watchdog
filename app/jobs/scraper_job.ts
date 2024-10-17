1e59d9f1e777621619f279111640d0240c9c6f55
artem <art3mk4@gmail.com>
2024-10-17 15:57:41 +0600

    2gis formatter, linter

--- app/jobs/scraper_job.ts
+++ app/jobs/scraper_job.ts
@@ -1,13 +1,13 @@
 import { BaseJob } from 'adonis-resque'
 import puppeteer from 'puppeteer-extra'
 import logger from '@adonisjs/core/services/logger'
-import env from "#start/env"
+import env from '#start/env'
 import stealthPlugin from 'puppeteer-extra-plugin-stealth'
 
 interface ItemData {
-  name: string,
-  text: string,
-  date: string,
+  name: string
+  text: string
+  date: string
   rating: number | null
 }
 
@@ -40,22 +40,22 @@ export default class ScraperJob extends BaseJob {
       headless: false,
       executablePath: puppeteer.executablePath(),
       args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-        "--disable-gpu",
-        "--incognito",
-        "--disable-client-side-phishing-detection",
-        "--disable-software-rasterizer",
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+        '--incognito',
+        '--disable-client-side-phishing-detection',
+        '--disable-software-rasterizer',
       ],
     }
     const url = env.get('PARSE_URL_2GIS')
 
-    const browser = await puppeteer.launch(launchOptions);
+    const browser = await puppeteer.launch(launchOptions)
     try {
       const page = await browser.newPage()
-      const userAgent =  env.get('USER_AGENT')
-      await page.setViewport({width: 1920, height: 1080})
+      const userAgent = env.get('USER_AGENT')
+      await page.setViewport({ width: 1920, height: 1080 })
       await page.setUserAgent(`${userAgent}`)
       await page.goto(`${url}`)
       await page.waitForNetworkIdle()
@@ -65,21 +65,36 @@ export default class ScraperJob extends BaseJob {
       logger.info('make screenshot')
 
       const items: ItemData[] = await page.evaluate(() => {
-        return [...Object.values(document.querySelectorAll('._h3pmwn')).map((item) => {
-          return {
-            name: item.parentElement.parentElement.parentElement.querySelector('._16s5yj36').childNodes[0].data || '',
-            text:  item.childNodes[0].data || '',
-            date: item.parentElement.parentElement.parentElement.querySelector('._139ll30').childNodes[0].data || '',
-            rating: item.parentElement.parentElement.parentElement.querySelector('._1fkin5c').childNodes.length || null
-          }
-        }), ...Object.values(document.querySelectorAll('._1oir7fah')).map((item) => {
-          return {
-            name: item.parentElement.parentElement.parentElement.querySelector('._16s5yj36').childNodes[0].data || '',
-            text:  item.childNodes[0].data || '',
-            date: item.parentElement.parentElement.parentElement.querySelector('._139ll30').childNodes[0].data || '',
-            rating: item.parentElement.parentElement.parentElement.querySelector('._1fkin5c').childNodes.length || null
-          }
-        })]
+        return [
+          ...Object.values(document.querySelectorAll('._h3pmwn')).map((item) => {
+            return {
+              name:
+                item.parentElement.parentElement.parentElement.querySelector('._16s5yj36')
+                  .childNodes[0].data || '',
+              text: item.childNodes[0].data || '',
+              date:
+                item.parentElement.parentElement.parentElement.querySelector('._139ll30')
+                  .childNodes[0].data || '',
+              rating:
+                item.parentElement.parentElement.parentElement.querySelector('._1fkin5c').childNodes
+                  .length || null,
+            }
+          }),
+          ...Object.values(document.querySelectorAll('._1oir7fah')).map((item) => {
+            return {
+              name:
+                item.parentElement.parentElement.parentElement.querySelector('._16s5yj36')
+                  .childNodes[0].data || '',
+              text: item.childNodes[0].data || '',
+              date:
+                item.parentElement.parentElement.parentElement.querySelector('._139ll30')
+                  .childNodes[0].data || '',
+              rating:
+                item.parentElement.parentElement.parentElement.querySelector('._1fkin5c').childNodes
+                  .length || null,
+            }
+          }),
+        ]
       })
 
       return items
@@ -105,17 +120,25 @@ export default class ScraperJob extends BaseJob {
   }
 
   createDateObject(dateString: string) {
-    const [day, month, year] = dateString.split(' ');
-    return new Date(Number(year), this.getMonthIndex(month), parseInt(day, 10));
+    const [day, month, year] = dateString.split(' ')
+    return new Date(Number(year), this.getMonthIndex(month), Number.parseInt(day, 10))
   }
 
   getMonthIndex(month: string) {
     const months = [
-      'января', 'февраля', 'марта',
-      'апреля', 'мая', 'июня',
-      'июля', 'августа', 'сентября',
-      'октября', 'ноября', 'декабря'
-    ];
+      'января',
+      'февраля',
+      'марта',
+      'апреля',
+      'мая',
+      'июня',
+      'июля',
+      'августа',
+      'сентября',
+      'октября',
+      'ноября',
+      'декабря',
+    ]
 
     return months.indexOf(month)
   }
